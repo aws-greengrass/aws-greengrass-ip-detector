98b1e7040d8a8bc60bca3f29055cf6d1dc1186f8
Kaustubh Khatri <mkkhpt@amazon.com>
2021-05-11 23:40:19 -0700

    feat: adding port config to ip detector

--- src/main/java/com/aws/greengrass/detector/uploader/ConnectivityUpdater.java
+++ src/main/java/com/aws/greengrass/detector/uploader/ConnectivityUpdater.java
@@ -2,6 +2,8 @@ package com.aws.greengrass.detector.uploader;
 
 import com.aws.greengrass.deployment.DeviceConfiguration;
 import com.aws.greengrass.detector.config.Config;
+import com.aws.greengrass.logging.api.Logger;
+import com.aws.greengrass.logging.impl.LogManager;
 import com.aws.greengrass.util.Coerce;
 import com.aws.greengrass.util.GreengrassServiceClientFactory;
 import lombok.NonNull;
@@ -20,6 +22,7 @@ public class ConnectivityUpdater {
     private final GreengrassServiceClientFactory clientFactory;
     private final Config config;
     private List<String> ipAddresses;
+    private final Logger logger = LogManager.getLogger(ConnectivityUpdater.class);
 
     /**
      * Constructor.
@@ -62,6 +65,7 @@ public class ConnectivityUpdater {
                 updateConnectivityInfo(connectivityInfoItems);
         if (connectivityInfoResponse != null && connectivityInfoResponse.version() != null) {
             this.ipAddresses = ips;
+            logger.atDebug().log("Connectivity information updated by ip detector");
         }
     }
 
