6cfb3322280bdf77bf22d9ef57ed3df2744fe4c2
Kaustubh Khatri <mkkhpt@amazon.com>
2021-05-16 22:26:44 -0700

    adding copyrights

--- src/test/java/com/aws/greengrass/detector/detector/IpDetectorTest.java
+++ src/test/java/com/aws/greengrass/detector/detector/IpDetectorTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.aws.greengrass.detector.detector;
 
 import com.aws.greengrass.utils.TestConstants;
